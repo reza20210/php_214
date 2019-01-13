f2ed92bf2c2360a84fb5b1023e9ed51dab246a86
Reza <rezaabasi20210@yahoo.com>
2019-01-13 11:02:12 -0800

    change Button to Search

--- resources/views/front/layouts/search.blade.php
+++ resources/views/front/layouts/search.blade.php
@@ -6,7 +6,7 @@
             <input name="search_key" type="text" class="form-control" placeholder="Search" aria-label="Search"
                    aria-describedby="button-addon2">
             <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Button</button>
+                <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
             </div>
         </div>
     </form>
